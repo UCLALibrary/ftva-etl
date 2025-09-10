4d8feb498f731c210b819dfc34345864173f738c
Henry R <90934334+henry-r18@users.noreply.github.com>
2025-09-10 14:36:29 -0700

    SYS-2001: Cast `inventory_id` to string in `filemaker.py` (#10)

--- src/ftva_etl/metadata/filemaker.py
+++ src/ftva_etl/metadata/filemaker.py
@@ -5,7 +5,7 @@ from fmrest.record import Record
 
 
 def get_inventory_id(fm_record: Record) -> str:
-    return fm_record.inventory_id
+    return str(fm_record.inventory_id)
 
 
 def get_inventory_number(fm_record: Record) -> str:
