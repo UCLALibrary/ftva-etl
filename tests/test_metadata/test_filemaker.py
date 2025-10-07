88b4d13759bfca975cd3c67fbb702ab3e653ead7
Henry Rosen <herosen@library.ucla.edu>
2025-10-07 15:32:09 -0700

    Add cleanup util for `production_type` strings
    * Update `is_series_production_type` to use new cleanup util
    * Update doc-strings
    * Update tests

--- tests/test_metadata/test_filemaker.py
+++ tests/test_metadata/test_filemaker.py
@@ -6,21 +6,37 @@ from fmrest.record import Record
 
 
 class TestFilemaker(TestCase):
-
-    def setUp(self):
-        valid_test_values = ["television series", "mini-series", "serials", "news"]
-        self.valid_test_records = [
-            Record(keys=["production_type"], values=[value])
-            for value in valid_test_values
-        ]
-
-        invalid_test_values = ["foo", "bar", "baz", "buz"]
-        self.invalid_test_records = [
-            Record(keys=["production_type"], values=[value])
-            for value in invalid_test_values
-        ]
-
     def test_is_series_production_type(self):
+        # Cherry-picked data from Filemaker PD that should be identified as series
+        valid_test_cases = [
+            "NEWS\rCOMPILATION",
+            "TELEVISION SERIES\rCOMEDY",
+            "MINI-SERIES\rDRAMA",
+            "SERIALS\rACTION",
+        ]
+        # These should not be identified as series
+        invalid_test_cases = [
+            "SILENT FILM\rEDUCATIONAL\rNEWSREELS\rCARTOONS",
+            "FOO\rBAR\rBAZ",
+            "TELEVISION\rMINIATURE",
+        ]
+        self.valid_test_records = [
+            # recordId and modId are added
+            # for use in __repr__ method on Record class
+            Record(
+                keys=["recordId", "modId", "production_type"],
+                values=[index, 0, value],
+            )
+            for index, value in enumerate(valid_test_cases)
+        ]
+        self.invalid_test_records = [
+            Record(
+                keys=["recordId", "modId", "production_type"],
+                values=[index, 0, value],
+            )
+            for index, value in enumerate(invalid_test_cases)
+        ]
+
         for record in self.valid_test_records:
             with self.subTest(record=record):
                 self.assertTrue(is_series_production_type(record))
