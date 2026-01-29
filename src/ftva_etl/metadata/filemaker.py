fc5a04c5f3d03a89057b3dcaa9dc513982d630ce
Henry Rosen <herosen@library.ucla.edu>
2026-01-29 15:15:43 -0800

    Map FM dates to metadata and return values as-is

--- src/ftva_etl/metadata/filemaker.py
+++ src/ftva_etl/metadata/filemaker.py
@@ -56,3 +56,24 @@ def get_language_name(fm_record: Record) -> str:
     :return: The language name as a string.
     """
     return fm_record.language
+
+
+def get_date_info(fm_record: Record) -> dict:
+    """Get the date info from a Filemaker record.
+
+    :param fm_record: A Filemaker record.
+    :return: A dict containing the date info.
+    """
+    # If FM data has `release_broadcast_year`, use it as the `release_broadcast_date`.
+    # Else, if FM data has `record_date`, use it as the `production_date`.
+    # Else, return a dict with `release_broadcast_date` as an empty string.
+    # NOTE: we're taking values as-is from FM here,
+    # mapping them to the date-like keys in the MAMS metadata.
+    date_info = {"release_broadcast_date": ""}
+
+    if fm_record.release_broadcast_year.strip() != "":
+        date_info["release_broadcast_date"] = fm_record.release_broadcast_year
+    elif fm_record.record_date.strip() != "":
+        date_info["production_date"] = fm_record.record_date
+
+    return date_info
