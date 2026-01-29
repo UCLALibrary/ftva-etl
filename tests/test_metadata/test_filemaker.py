87c9fc17934456556606a6d64a40d59df615b50c
Henry Rosen <herosen@library.ucla.edu>
2026-01-29 15:18:21 -0800

    Add tests for FM dates

--- tests/test_metadata/test_filemaker.py
+++ tests/test_metadata/test_filemaker.py
@@ -45,3 +45,57 @@ class TestFilemaker(TestCase):
         for record in self.invalid_test_records:
             with self.subTest(record=record):
                 self.assertFalse(is_series_production_type(record))
+
+
+class TestFilemakerDateInfo(TestCase):
+    def setUp(self):
+        # Test cases are tuples, where 1st elem is input date info,
+        # and 2nd elem is expected result from get_date_info()
+        test_cases = [
+            # Summary of specs:
+            # - If release_broadcast_year is not empty, parse it as the release_broadcast_date;
+            # - else if record_date is not empty, parse it as the production_date.
+            # - If neither is present, return a dict with release_broadcast_date as an empty string.
+            # - In all cases, release_broadcast_date should be present in the output dict,
+            # but production_date should be present only if it is not empty.
+            (
+                {
+                    "release_broadcast_year": "1997",
+                    "record_date": "1997-12-02",
+                },
+                {"release_broadcast_date": "1997"},
+            ),
+            # only release_broadcast_year present
+            (
+                {"release_broadcast_year": "1997", "record_date": ""},
+                {"release_broadcast_date": "1997"},
+            ),
+            # only record_date present
+            (
+                {"release_broadcast_year": "", "record_date": "11/9/2004"},
+                {"release_broadcast_date": "", "production_date": "11/9/2004"},
+            ),
+        ]
+
+        # Transform test cases into tuples of FM record and expected results
+        self.test_records = [
+            (
+                Record(
+                    keys=["recordId", "modId", "release_broadcast_year", "record_date"],
+                    values=[
+                        index,
+                        0,
+                        test_case[0]["release_broadcast_year"],
+                        test_case[0]["record_date"],
+                    ],
+                ),
+                test_case[1],  # expected result
+            )
+            for index, test_case in enumerate(test_cases)
+        ]
+
+    def test_get_date_info(self):
+        """Test that `get_date_info` correctly extracts date info from FM records."""
+        for record, expected_result in self.test_records:
+            with self.subTest(record=record):
+                self.assertDictEqual(get_date_info(record), expected_result)
