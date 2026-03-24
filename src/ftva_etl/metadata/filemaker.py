ddcced51c5086a7bd20827144c93c56834cd0955
Zoe Tucker <zoetucker@library.ucla.edu>
2026-03-24 12:56:14 -0700

    DLSR-153: merge legacy_fm_changes into main (#29)
    * SYS-2139: Add FM-only `creators` to metadata (#20)
    * SYS-2146: Add ability to get metadata for DD-FM-only records (#23)
    * SYS-2137: get Title info from FM (and merge changes from main) (#25)
    * DLSR-26: fix FM title-related field names in ETL package (#27)
    Co-authored-by: Henry R <90934334+henry-r18@users.noreply.github.com>

--- src/ftva_etl/metadata/filemaker.py
+++ src/ftva_etl/metadata/filemaker.py
@@ -1,8 +1,12 @@
 from fmrest.record import Record
 from .utils import cleanup_production_type
+import logging
+
 
 # Code which extracts data from a Filemaker record.
-# Minimal for now.
+
+# Create a module logger, which will be a child of the package logger
+logger = logging.getLogger(__name__)
 
 
 def get_inventory_id(fm_record: Record) -> str:
@@ -35,3 +39,123 @@ def is_series_production_type(fm_record: Record) -> bool:
     series_keywords = ["television series", "mini-series", "serials", "news"]
     # Look for any of the keywords in the production type list
     return any(keyword in production_type for keyword in series_keywords)
+
+
+def get_creators(fm_record: Record) -> list:
+    """Get the creators from a Filemaker record.
+
+    :param fm_record: A Filemaker record.
+    :return: A list of creators.
+    """
+    # Currently splitting on commas,
+    # as that appears to be how the data is formatted in Filemaker.
+    # If other delimiters are used, they should probably be made consistent on the FM side.
+    return [creator.strip() for creator in fm_record.director.split(",")]
+
+
+def get_language_name(fm_record: Record) -> str:
+    """Get the language name from a Filemaker record.
+
+    :param fm_record: A Filemaker record.
+    :return: The language name as a string.
+    """
+    # NOTE: field name is capitalized in FM
+    return fm_record.Language
+
+
+def get_date_info(fm_record: Record) -> dict:
+    """Get the date info from a Filemaker record.
+
+    :param fm_record: A Filemaker record.
+    :return: A dict containing the date info.
+    """
+    # If FM data has `release_broadcast_year`, map it to `release_broadcast_date`.
+    # Else, if FM data has `record_date`, map it to `production_date`.
+    # Else, return an empty dict.
+    # NOTE: we're taking values as-is from FM here,
+    # mapping them to the date-like keys in the MAMS metadata.
+    date_info = {}
+
+    if fm_record.release_broadcast_year.strip() != "":
+        date_info["release_broadcast_date"] = fm_record.release_broadcast_year
+    elif fm_record.record_date.strip() != "":
+        date_info["production_date"] = fm_record.record_date
+
+    return date_info
+
+
+def is_compilation(fm_record: Record) -> bool:
+    """Determine if the `production_type` field represents a compilation,
+    based on certain keywords defined in FTVA specs.
+
+    :param fm_record: A Filemaker record.
+    :return: True if the production type is a compilation, False otherwise.
+    """
+
+    production_type = cleanup_production_type(fm_record.production_type)
+    title = fm_record.title.lower()
+    if "compilation" in production_type or "compilation" in title:
+        return True
+    return False
+
+
+def get_title_info(fm_record: Record, is_series: bool) -> dict:
+    """Get the title info from a Filemaker record.
+
+    :param fm_record: A Filemaker record.
+    :param is_series: Whether the record is a series.
+    :return: A dict containing the title info.
+    """
+    if is_compilation(fm_record):
+        logger.info(
+            f"Record with inventory id {fm_record.inventory_id} is identified as a compilation. "
+            f"Production type: {fm_record.production_type}, title: {fm_record.title}"
+        )
+        return {}
+
+    fm_title = fm_record.title.strip()
+    # If no title found, log a warning and return an empty dict.
+    if not fm_title:
+        logger.warning(
+            f"Record with inventory id {fm_record.inventory_id} has no title. "
+        )
+        return {}
+    # If not a series, just return the title as-is.
+    if not is_series:
+        return {"title": fm_record.title}
+
+    # For series, we want the series title, episode title, and episode number (if available).
+
+    fm_ep_title = fm_record.episode_title.strip()
+    # Space in field name means we have to use dict-style access to get the episode number field.
+    fm_ep_no = fm_record["episode no."].strip()
+
+    if fm_title and fm_ep_title and fm_ep_no:
+        series_title = fm_title
+        episode_title = fm_ep_title + ". " + fm_ep_no
+        title = series_title + ". " + episode_title
+        return {
+            "title": title,
+            "series_title": series_title,
+            "episode_title": episode_title,
+        }
+    elif fm_title and fm_ep_title:
+        series_title = fm_title
+        episode_title = fm_ep_title
+        title = series_title + ". " + episode_title
+        return {
+            "title": title,
+            "series_title": series_title,
+            "episode_title": episode_title,
+        }
+    elif fm_title and fm_ep_no:
+        series_title = fm_title
+        episode_title = fm_ep_no
+        title = series_title + ". " + episode_title
+        return {
+            "title": title,
+            "series_title": series_title,
+            "episode_title": episode_title,
+        }
+    else:
+        return {"title": fm_title}
--- src/ftva_etl/metadata/mams_metadata.py
+++ src/ftva_etl/metadata/mams_metadata.py
@@ -1,6 +1,10 @@
 import spacy
+import logging
+
 from fmrest.record import Record as FM_Record
 from pymarc import Record as Pymarc_Record
+from typing import Optional
+from spacy.language import Language
 from .digital_data import (
     get_asset_type,
     get_dcp_info,
@@ -10,27 +14,86 @@ from .digital_data import (
     get_uuid,
     get_audio_class,
 )
-from .filemaker import get_inventory_id, get_inventory_number, is_series_production_type
+from .filemaker import (
+    get_inventory_id,
+    get_inventory_number,
+    is_series_production_type,
+    get_creators as get_fm_creators,
+    get_date_info as get_fm_date_info,
+    get_language_name as get_fm_language_name,
+    get_title_info as get_fm_title_info,
+)
 from .marc import (
     get_bib_id,
-    get_creators,
-    get_date_info,
-    get_language_name,
-    get_title_info,
+    get_creators as get_alma_creators,
+    get_date_info as get_alma_date_info,
+    get_language_name as get_alma_language_name,
+    get_title_info as get_alma_title_info,
 )
 
 
-def get_mams_metadata(
-    bib_record: Pymarc_Record,
-    filemaker_record: FM_Record,
-    digital_data_record: dict,
-    match_asset_uuid: str | None = None,
-) -> dict:
-    """Generate JSON metadata for ingest into the FTVA MAMS.
+# Create a module logger, which will be a child of the package logger
+logger = logging.getLogger(__name__)
 
-    :param bib_record: A pymarc record, expected to contain bibliographic data.
-    :param filemaker_record: A fmrest filemaker record.
+
+def get_alma_metadata(
+    bib_record: Pymarc_Record, nlp_model: Language, is_series: bool = False
+) -> dict:
+    """Get the Alma metadata from a pymarc record.
+
+    :param bib_record: A pymarc record.
+    :param nlp_model: A spacy language model to use for NER.
+    :param is_series: Whether the record is a series, derived from Filemaker record.
+        Defaults to False.
+    :return: A dict containing the Alma metadata needed for the MAMS.
+    """
+    # This gets a collection of titles which will be unpacked later.
+    titles = get_alma_title_info(bib_record, is_series)
+    return {
+        "alma_bib_id": get_bib_id(bib_record),
+        "language": get_alma_language_name(bib_record),
+        "creators": get_alma_creators(bib_record, nlp_model),
+        **titles,
+        **get_alma_date_info(bib_record),
+    }
+
+
+def get_filemaker_metadata(filemaker_record: FM_Record, is_series: bool) -> dict:
+    """Get the Filemaker metadata from a Filemaker record.
+
+    :param filemaker_record: A Filemaker record.
+    :param is_series: Whether the record is a series, derived from Filemaker record.
+    :return: A dict containing the Filemaker metadata needed for the MAMS.
+    """
+    titles = get_fm_title_info(filemaker_record, is_series)
+    return {
+        "inventory_id": get_inventory_id(filemaker_record),
+        # All records returned from FM
+        # should have only one inventory number for now,
+        # but MAMS expects an array in JSON, so wrap in a list.
+        # TODO: Parse comma-separated or otherwise delimited inventory numbers
+        # from FM or other sources, if needed.
+        "inventory_numbers": [get_inventory_number(filemaker_record)],
+        "creators": get_fm_creators(filemaker_record),
+        "language": get_fm_language_name(filemaker_record),
+        **titles,
+        **get_fm_date_info(filemaker_record),
+    }
+
+
+def _get_source_metadata(
+    digital_data_record: dict,
+    filemaker_record: FM_Record,
+    bib_record: Optional[Pymarc_Record] = None,
+    match_asset_uuid: Optional[str] = None,
+) -> dict:
+    """Retrieve and combine metadata from the source records into a single dict, for
+    further processing and eventual output to the MAMS.
+
     :param digital_data_record: A dict containing an FTVA digital data record.
+    :param filemaker_record: A fmrest filemaker record.
+    :param bib_record: A pymarc record, expected to contain bibliographic data.
+        Optional to support multiple types of matching (e.g. DD-FM-Alma or DD-FM only).
     :param match_asset_uuid: A string representation of an asset's UUID. Defaults to None.
     :return asset: A dict of metadata combined from the input records.
     """
@@ -41,34 +104,24 @@ def get_mams_metadata(
     # may be for batch processing.
     nlp_model = spacy.load("en_core_web_md")
 
-    # Used for some special handling of serial titles
+    # Determine if the record is a series, as determined by the Filemaker record.
     is_series = is_series_production_type(filemaker_record)
 
-    # This gets a collection of titles which will be unpacked later.
-    titles = get_title_info(bib_record, is_series)
+    filemaker_metadata = get_filemaker_metadata(filemaker_record, is_series)
 
-    # Get the date and qualifier.
-    date_info = get_date_info(bib_record)
+    alma_metadata = (
+        get_alma_metadata(bib_record, nlp_model, is_series) if bib_record else {}
+    )
 
     # Get the rest of the data and prepare it for return.
     metadata = {
-        "alma_bib_id": get_bib_id(bib_record),
-        "inventory_id": get_inventory_id(filemaker_record),
+        "alma": alma_metadata,
+        "filemaker": filemaker_metadata,
         "uuid": get_uuid(digital_data_record),
-        # All records returned from FM
-        # should have only one inventory number for now,
-        # but MAMS expects an array in JSON, so wrap in a list.
-        # TODO: Parse comma-separated or otherwise delimited inventory numbers
-        # from FM or other sources, if needed.
-        "inventory_numbers": [get_inventory_number(filemaker_record)],
-        "creators": get_creators(bib_record, nlp_model),
-        "language": get_language_name(bib_record),
         "file_name": get_file_name(digital_data_record),
         "asset_type": get_asset_type(digital_data_record),
         "media_type": get_media_type(digital_data_record),
         "audio_class": get_audio_class(digital_data_record),
-        **titles,
-        **date_info,
     }
 
     # If a match_asset_uuid is provided for a track, add it to the metadata record.
@@ -84,5 +137,72 @@ def get_mams_metadata(
     else:
         # No special action needed
         pass
+
+    return metadata
+
+
+def _extract_source_metadata(source_metadata: dict, type: str) -> dict:
+    """Extract the relevant metadata for a given type (e.g. alma, filemaker) from the
+    combined source metadata dict.
+
+    :param source_metadata: A dict containing the combined source metadata.
+    :param type: The type of metadata to extract (e.g. "alma", "filemaker").
+    :return: A dict containing unpacked metadata for the given type.
+    """
+    output = {}
+    output["creators"] = source_metadata[type].get("creators", [])
+    output["language"] = source_metadata[type].get("language", "")
+    # Get all fields with "title" in the name (e.g. title, series_title).
+    output.update({k: v for k, v in source_metadata[type].items() if "title" in k})
+    # Get all date fields (e.g. release_broadcast_date, distribution_date).
+    output.update({k: v for k, v in source_metadata[type].items() if "date" in k})
+    return output
+
+
+def get_mams_metadata(
+    digital_data_record: dict,
+    filemaker_record: FM_Record,
+    bib_record: Optional[Pymarc_Record] = None,
+    match_asset_uuid: Optional[str] = None,
+) -> dict:
+    """Format the source metadata for output to the MAMS.
+
+    :param digital_data_record: A dict containing an FTVA digital data record.
+    :param filemaker_record: A fmrest filemaker record.
+    :param bib_record: A pymarc record, expected to contain bibliographic data.
+        Optional to support multiple types of matching (e.g. DD-FM-Alma or DD-FM only).
+    :param match_asset_uuid: A string representation of an asset's UUID. Defaults to None.
+    :return: A dict containing the metadata formatted for output to the MAMS.
+    """
+    source_metadata = _get_source_metadata(
+        digital_data_record, filemaker_record, bib_record, match_asset_uuid
+    )
+    # Begin structuring metadata for MAMS output.
+    # For now, always assume we have a Filemaker record.
+    metadata = {
+        "inventory_id": source_metadata["filemaker"]["inventory_id"],
+        "uuid": source_metadata["uuid"],
+        "inventory_numbers": source_metadata["filemaker"]["inventory_numbers"],
+        "file_name": source_metadata["file_name"],
+        "asset_type": source_metadata["asset_type"],
+        "media_type": source_metadata["media_type"],
+        "audio_class": source_metadata["audio_class"],
+    }
+    # Folder and subfolder may be empty, depending on file type.
+    # Match Asset may also be empty depending on asset/track relationships and matching results.
+    # Only include if there is a value.
+    if source_metadata.get("folder_name"):
+        metadata["folder_name"] = source_metadata["folder_name"]
+    if source_metadata.get("sub_folder_name"):
+        metadata["sub_folder_name"] = source_metadata["sub_folder_name"]
+    if source_metadata.get("match_asset"):
+        metadata["match_asset"] = source_metadata["match_asset"]
+
+    # 1-1-1 match: if we have Alma metadata, it is preferred over FM for certain fields.
+    if source_metadata.get("alma"):
+        metadata["alma_bib_id"] = source_metadata["alma"].get("alma_bib_id", "")
+        metadata.update(_extract_source_metadata(source_metadata, "alma"))
+    else:
+        metadata.update(_extract_source_metadata(source_metadata, "filemaker"))
 
     return metadata
