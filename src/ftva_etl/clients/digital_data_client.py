11f951d0f04bd67aa2cb597e2b8c709eda1f56ae
Henry Rosen <herosen@library.ucla.edu>
2026-06-16 17:37:14 -0700

    Add wrapper for `get_records` endpoint on Digital Data app

--- src/ftva_etl/clients/digital_data_client.py
+++ src/ftva_etl/clients/digital_data_client.py
@@ -21,6 +21,38 @@ class DigitalDataClient:
         url = f"{self._url}/records/{record_id}"
         return self._get_record(url)
 
+    def get_records(
+        self,
+        offset: int | None = None,
+        limit: int | None = None,
+        query: str = "",
+        fields: list[str] | None = None,
+    ) -> dict:
+        """Get FTVA Digital Data records with optional filtering and pagination.
+
+        :param offset: Starting record for pagination. Defaults are applied by the API.
+        :param limit: Number of results to return. Defaults are applied by the API.
+        :param query: Optional search/filter string.
+        :param fields: Optional list of fields to search in.
+        :return: JSON from the response, containing records and total_records.
+        """
+        url = f"{self._url}/records/"
+        params: dict[str, int | str] = {}
+        # All params are optional, so set them only if they are provided.
+        if offset:
+            params["offset"] = offset
+        if limit:
+            params["limit"] = limit
+        if query:
+            params["query"] = query
+        # Fields expected as a comma-separated string.
+        if fields:
+            params["fields"] = ",".join(fields)
+
+        response = requests.get(url, auth=(self._user, self._password), params=params)
+        response.raise_for_status()
+        return response.json()
+
     def _get_record(self, url: str) -> dict:
         """General routine for fetching data.
 
