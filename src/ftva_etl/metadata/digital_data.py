feb5747b3cdc3992b459721a2f431465ccc009aa
Henry R <90934334+henry-r18@users.noreply.github.com>
2026-06-24 16:43:14 -0700

    DLSR-414: Refactor `mams_metadata.py` and handling of `record_type`, `match_asset`, and `file_type` fields (#35)
    * Handle `record_type` and `match_asset` fields
    * Add function for getting `record_type` and `match_asset` fields from DD records
    * Handle `file_type` field for DCP and DPX
    * Refactor `mams_metadata`
    * Combine `get_mams_metadata` and `_get_source_metadata` into a single function
    * Address TODO related to spacy NLP model, allowing caller to optionally pass it in
    * Rename `_extract_descriptive_metadata` to `_get_descriptive_metadata`
    * Bump package version to `0.5.0`

--- src/ftva_etl/metadata/digital_data.py
+++ src/ftva_etl/metadata/digital_data.py
@@ -37,11 +37,14 @@ def get_dcp_info(dd_record: dict) -> dict:
     :param dd_record: A Digital Data record
     :return: A dictionary of fields.
     """
+    if dd_record.get("file_type", "") != "DCP":
+        return {}
     return {
         # File name must always be empty for DCPs.
         "file_name": "",
         "folder_name": get_folder_name(dd_record),
         "sub_folder_name": get_sub_folder_name(dd_record),
+        "file_type": "DCP",  # file type required by MAMS for DCP files
     }
 
 
@@ -51,11 +54,13 @@ def get_dpx_info(dd_record: dict) -> dict:
     :param dd_record: A Digital Data record
     :return: A dictionary of fields.
     """
+    if dd_record.get("file_type", "") != "DPX":
+        return {}
     return {
         # File name must always be empty for DPXs.
         "file_name": "",
         "folder_name": get_folder_name(dd_record),
-        "file_type": "DPX",  # Required by MAMS only for DPX files. Hard-coded here.
+        "file_type": "DPX",  # file type required by MAMS for DPX files
     }
 
 
@@ -66,3 +71,34 @@ def get_audio_class(dd_record: dict) -> str:
     :return: The audio class, or an empty string if not present.
     """
     return dd_record.get("audio_class", "")
+
+
+def get_record_type_and_match_asset(dd_record: dict) -> dict:
+    """Return the record type for a Digital Data record,
+    and if applicable, the match asset for tracks.
+
+    :param dd_record: A Digital Data record
+    :return: A dictionary containing the record type,
+    and if applicable, the match asset for tracks.
+    """
+    # Check if the DD record is a track,
+    # indicated by an incoming relationship with a type of `isTrackOf`
+    incoming_relationships = dd_record.get("incoming_relationships", [])
+    # Get the first relationship with a type of `isTrackOf`,
+    # or None if no such relationship is found.
+    track_relationship = next(
+        (
+            relationship
+            for relationship in incoming_relationships
+            if relationship.get("relationship_type", "") == "isTrackOf"
+        ),
+        None,
+    )
+    # If a track relationship is found, set `record_type=track`
+    # and `match_asset` to the source UUID for the relationship.
+    if track_relationship:
+        return {
+            "record_type": "track",
+            "match_asset": track_relationship.get("source_uuid"),
+        }
+    return {"record_type": "asset"}
